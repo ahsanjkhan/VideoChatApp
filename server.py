14d91a2d41b8f8aa11e129f913733906cfc081b9
Ahsan Khan <ahsanjkhan7@gmail.com>
2023-02-23 00:28:53 -0500

    testing branches #1

--- server.py
+++ server.py
@@ -16,4 +16,5 @@ def registerPOST():
 
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    #testing branches
     app.run(host='0.0.0.0', port=port, debug=True)
